319ac714ed2097e1f6d1f385b3a8f50c992a5081
Farid Gasimov <farid.gasimov95@gmail.com>
2026-09-21 21:25:53 +0400

    fix: scroll to the top of the page after navigating bug fixed

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,12 +4,14 @@ import Header from "./Header";
 import { LanguageProvider } from "../../context/languageContext";
 import Footer from "./Footer";
 import IconSprite from "./IconSprite";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout() {
   return (
     <StrictMode>
       <LanguageProvider>
         <IconSprite />
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
